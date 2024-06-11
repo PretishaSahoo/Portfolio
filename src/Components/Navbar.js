2b00202ff6f468c74e0ddc76a207f05849f38e58
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-06-11 23:33:07 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,9 +9,9 @@ import './Styles.css';
 export default function Navbar() {
   return (
     <>
-    <header className="text-gray-50 fixed top-0 left-0 right-0 w-full z-10 bg-opacity-90 backdrop-filter backdrop-blur-lg shadow-lg  mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center">
-       <a href="#" className="flex items-center whitespace-nowrap text-2xl font-black">
-          <p className='p-2 m-2'>Pretisha Sahoo</p>
+    <header className="text-gray-50 fixed top-0 left-0 right-0 w-full z-10 bg-opacity-90 backdrop-filter backdrop-blur-lg shadow-lg  mx-auto flex flex-col overflow-hidden px-4 py-4 lg:flex-row lg:items-center bg-transparent">
+       <a href="#" className="flex items-center whitespace-nowrap text-2xl  bg-transparent  ">
+          <p className='p-2 m-2 bg-transparent'>Pretisha Sahoo</p>
       </a>
       <input type="checkbox" className="peer hidden" id="navbar-open" />
       <label className="absolute top-5 right-5 cursor-pointer lg:hidden" htmlFor="navbar-open">
@@ -19,22 +19,22 @@ export default function Navbar() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </label>
-      <nav aria-label="Header Navigation" className="p-2 peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
-        <ul className="flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0 lg:space-x-12">
-          <li className="lg:mr-12">
-            <a className="rounded-xl p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#About">About</a>
+      <nav aria-label="Header Navigation" className="p-2 peer-checked:pt-8 peer-checked:max-h-60 flex max-h-0 w-full flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row bg-transparent">
+        <ul className=" flex w-full flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0 lg:space-x-12 bg-transparent">
+          <li className="lg:mr-12 bg-transparent">
+            <a className="rounded-xl bg-transparent p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#About">About</a>
           </li>
-          <li className="lg:mr-12">
-            <a className="rounded-xl p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Skills">Skills</a>
+          <li className="lg:mr-12 bg-transparent ">
+            <a className="rounded-xl bg-transparent p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Skills">Skills</a>
           </li>
-          <li className="lg:mr-12">
-            <a className="rounded-xl p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Projects">Projects</a>
+          <li className="lg:mr-12 bg-transparent ">
+            <a className="rounded-xl bg-transparent p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Projects">Projects</a>
           </li>
-          <li className="lg:mr-12">
-            <a className="rounded-xl p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Education">Education</a>
+          <li className="lg:mr-12 bg-transparent">
+            <a className="rounded-xl bg-transparent p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Education">Education</a>
           </li>
-          <li className="lg:mr-12">
-            <a className="rounded-xl p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Contact">Contact</a>
+          <li className="lg:mr-12 bg-transparent">
+            <a className="rounded-xl bg-transparent p-2 text-gray-50 transition hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-offset-2" href="#Contact">Contact</a>
           </li>
         </ul>
         <hr className="mt-4 w-full lg:hidden" />
