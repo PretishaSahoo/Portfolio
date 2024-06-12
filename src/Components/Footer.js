193da200262f558e7441e6ef8b9925684698e251
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-06-12 10:06:09 +0530

    Footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto">
+        <span className="primary-color ">Pretisha Sahoo</span>
+        <p className="text-gray-600">pretishasahoo@gmail.com  <br /> Up for innovative Projects <br /> @2024</p>
+    </div>
+  )
+}
