b8d041b1983322d44dc5956e3e422cd356ed7b4f
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-06-12 10:06:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App h-screen bg-black ">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
