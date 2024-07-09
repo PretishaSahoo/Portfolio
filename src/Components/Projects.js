f03e5ce07ab6584b8200df9cf544911e30faef3c
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-07-09 08:37:17 +0530

    new adds

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,6 +7,7 @@ import PlayScape from '../Images/PlayScape.png'
 import Twilight from '../Images/Twilight.png'
 import TextUtils from '../Images/TextUtils.png'
 import Newsmate from '../Images/Newsmate.png'
+import StudyGenie from "../Images/StudyGenie.png"
 
 export default function Projects() {
   return (
@@ -65,6 +66,18 @@ export default function Projects() {
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center bg-transparent">
                 <a  className="bg-transparent" target="blank" href="https://cloud-teller.vercel.app/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
+                </a>
+              </div>
+            </div>
+        </div>
+
+        <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
+          <img src={StudyGenie} layout="fill" objectFit="cover" alt="" />
+            <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+              <span className="text-2xl font-bold text-white tracking-wider"></span>
+              <div className="pt-8 text-center bg-transparent">
+                <a  className="bg-transparent" target="blank" href="https://studygenie-cf.vercel.app/">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
                 </a>
               </div>
