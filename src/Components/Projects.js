296870f5d9757d52f7c8cdf2143bd0ddace25c3b
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-07-24 23:10:33 +0530

    updates

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,6 +8,7 @@ import Twilight from '../Images/Twilight.png'
 import TextUtils from '../Images/TextUtils.png'
 import Newsmate from '../Images/Newsmate.png'
 import StudyGenie from "../Images/StudyGenie.png"
+import ShopNest from '../Images/ShopNest.png'
 
 export default function Projects() {
   return (
@@ -42,6 +43,18 @@ export default function Projects() {
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center bg-transparent">
                 <a  className="bg-transparent" target="blank" href="https://chitchat-pro.vercel.app/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
+                </a>
+              </div>
+            </div>
+        </div>
+
+        <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
+          <img src={ShopNest} layout="fill" objectFit="cover" alt="" />
+            <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+              <span className="text-2xl font-bold text-white tracking-wider"></span>
+              <div className="pt-8 text-center bg-transparent">
+                <a  className="bg-transparent" target="blank" href="https://shop-nest-olive.vercel.app/">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
                 </a>
               </div>
@@ -95,7 +108,7 @@ export default function Projects() {
               </div>
             </div>
         </div>
-{/* 
+
         <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
           <img src={Twilight} layout="fill" objectFit="cover" alt="" />
             <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
@@ -106,7 +119,7 @@ export default function Projects() {
                 </a>
               </div>
             </div>
-        </div> */}
+        </div>
 
         <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
           <img src={TextUtils} layout="fill" objectFit="cover" alt="" />
