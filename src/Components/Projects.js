17f478571dd871ad312b59db2bec3cc6329226b5
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-08-12 15:09:38 +0530

    HealthAI added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,6 +9,7 @@ import TextUtils from '../Images/TextUtils.png'
 import Newsmate from '../Images/Newsmate.png'
 import StudyGenie from "../Images/StudyGenie.png"
 import ShopNest from '../Images/ShopNest.png'
+import HealthAI from "../Images/HealthAI.png"
 
 export default function Projects() {
   return (
@@ -24,6 +25,18 @@ export default function Projects() {
       </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+
+      <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
+          <img src={ShopNest} layout="fill" objectFit="cover" alt="" />
+            <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+              <span className="text-2xl font-bold text-white tracking-wider"></span>
+              <div className="pt-8 text-center bg-transparent">
+                <a  className="bg-transparent" target="blank" href="https://shop-nest-olive.vercel.app/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
+                </a>
+              </div>
+            </div>
+        </div>
 
         <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
           <img src={repxAI} layout="fill" objectFit="cover" alt="" />
@@ -50,11 +63,11 @@ export default function Projects() {
         </div>
 
         <div className="rounded-md transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#0404c16] group container mounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
-          <img src={ShopNest} layout="fill" objectFit="cover" alt="" />
+          <img src={HealthAI} layout="fill" objectFit="cover" alt="" />
             <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center bg-transparent">
-                <a  className="bg-transparent" target="blank" href="https://shop-nest-olive.vercel.app/">
+                <a  className="bg-transparent" target="blank" href="https://health-ai-teal.vercel.app/">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-red-300 text-gray-700 font-bold text-lg">Check Out!</button>
                 </a>
               </div>
